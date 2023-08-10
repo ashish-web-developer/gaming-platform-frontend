cf97dae6de760e3f06459c588e59435d2b088f58
= <=>
2023-08-10 20:20:21 +0530

    [chat-functionality] added styles to chat messages

--- components/chat/chat-message.tsx
+++ components/chat/chat-message.tsx
@@ -1,8 +1,10 @@
 //types
+import type Colors from "@/types/data/colors";
 import type { FC } from "react";
 
 // styled components
 import {
+  StyledChatParentContainer,
   StyledChatMessageContainer,
   StyledChatMessageUserName,
   StyledChatMessage,
@@ -10,23 +12,49 @@ import {
   StyledChatTime,
 } from "@/styles/components/chat/chat-message.style";
 
+// local components
+import ChatAvatar from "@/components/chat/chat-avatar";
+
+// hooks
+import useAvatar from "@/hooks/profile";
 
 interface Props {
   message: string;
-  date:string;
-  username:string
-  align:string
+  date: string;
+  username: string;
+  name: string;
+  align: string;
+  colors: Colors;
+  flexDirection: "row" | "row-reverse";
 }
-const ChatMessage: FC<Props> = ({ message , date, username,align}) => {
+const ChatMessage: FC<Props> = ({
+  message,
+  date,
+  username,
+  name,
+  align,
+  colors,
+  flexDirection,
+}) => {
+  const avatar = useAvatar(username ?? "");
+  const color = useColor(colors);
   return (
-    <StyledChatMessageContainer $align = {align}>
-      <StyledChatMessageUserName>{username}</StyledChatMessageUserName>
-      <StyledChatMessage>{message}</StyledChatMessage>
-      <StyledChatBottom>
-        <StyledChatTime>{date}</StyledChatTime>
-      </StyledChatBottom>
-    </StyledChatMessageContainer>
+    <StyledChatParentContainer $flexDirection={flexDirection} $align={align}>
+      <ChatAvatar color={color} width={60} height={60} avatar={avatar} />
+      <StyledChatMessageContainer>
+        <StyledChatMessageUserName>{name}</StyledChatMessageUserName>
+        <StyledChatMessage>{message}</StyledChatMessage>
+        <StyledChatBottom>
+          <StyledChatTime>{date}</StyledChatTime>
+        </StyledChatBottom>
+      </StyledChatMessageContainer>
+    </StyledChatParentContainer>
   );
+};
+
+const useColor = (colors: Colors) => {
+  const colorsLength = colors.length;
+  return colors[Math.floor(Math.random() * colorsLength)];
 };
 
 export default ChatMessage;
