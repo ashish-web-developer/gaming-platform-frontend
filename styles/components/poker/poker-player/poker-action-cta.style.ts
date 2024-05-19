56b21aa40148558bf36aabb86baadde8d6a9d9b3
= <=>
2024-05-19 10:18:08 +0530

    [poker] added text inside poker action cta

--- styles/components/poker/poker-player/poker-action-cta.style.ts
+++ styles/components/poker/poker-player/poker-action-cta.style.ts
@@ -12,15 +12,14 @@ const StyledVectorWrapper = styled.div<{
   $right?: string;
   $top?: string;
   $bottom?: string;
+  $cursor?: "auto" | "pointer";
 }>`
   position: absolute;
   left: ${(props) => props.$left ?? "auto"};
   right: ${(props) => props.$right ?? "auto"};
   top: ${(props) => props.$top ?? "auto"};
   bottom: ${(props) => props.$bottom ?? "auto"};
+  cursor: ${(props) => props.$cursor ?? "auto"};
 `;
 
-export {
-  StyledPokerActionCta,
-  StyledVectorWrapper,
-};
+export { StyledPokerActionCta, StyledVectorWrapper };
