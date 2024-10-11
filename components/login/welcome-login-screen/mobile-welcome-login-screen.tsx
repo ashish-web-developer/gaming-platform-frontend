3fba745be7a8f6ed5b6f15227d4746ed6060111e
= <=>
2024-10-11 12:25:47 +0530

    [login-mobile-view] added logo animation

--- components/login/welcome-login-screen/mobile-welcome-login-screen.tsx
+++ components/login/welcome-login-screen/mobile-welcome-login-screen.tsx
@@ -1,8 +1,6 @@
+import { useEffect, useRef } from "react";
 // types
 import type { FC } from "react";
-
-// theme
-import { useTheme } from "styled-components";
 
 // local components
 import MobileGameBanner from "@/components/login/game-banner/mobile-game-banner";
@@ -21,10 +19,62 @@ import {
 // vector
 import CtaVector from "@/components/login/vector/cta-vector";
 
+// gsap
+import gsap from "gsap-trial";
+import SplitText from "gsap-trial/SplitText";
+
+gsap.registerPlugin(SplitText);
+
 const MobileWelcomeLoginScreen: FC = () => {
-  const theme = useTheme();
+  const container_ref = useRef<HTMLDivElement>(null);
+  const logo_ref = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const split_text = new SplitText(logo_ref.current, { type: "words,chars" });
+    const gsap_context = gsap.context(() => {
+      gsap
+        .timeline()
+        .from(split_text.chars, {
+          delay: 1,
+          duration: 1,
+          scale: 1.2,
+          stagger: 0.1,
+          ease: "power2.out",
+          onComplete: () => {
+            split_text.revert();
+          },
+        })
+        .to(logo_ref.current, {
+          rotate: -5,
+          duration: 0.6,
+          ease: "bounce",
+        })
+        .to(
+          ".subtitle",
+          {
+            rotate: -5,
+            duration: 0.6,
+            ease: "bounce",
+          },
+          "<"
+        )
+        .to(
+          ".unlock-cta",
+          {
+            rotate: 0,
+            duration: 0.6,
+            ease: "bounce",
+          },
+          "<"
+        );
+    }, container_ref);
+    return () => {
+      split_text.revert();
+      gsap_context.revert();
+    };
+  }, []);
   return (
-    <StyledPage>
+    <StyledPage ref={container_ref}>
       <MobileGameBanner
         banner_type="cognimatch"
         main_color={"#F42C04"}
@@ -39,12 +89,12 @@ const MobileWelcomeLoginScreen: FC = () => {
       />
       <StyledBottomContentContainer>
         <StyledLogoContainer>
-          <StyledLogo>Fortune Realm</StyledLogo>
-          <StyledLogoSubtitle>
+          <StyledLogo ref={logo_ref}>Fortune Realm</StyledLogo>
+          <StyledLogoSubtitle className="subtitle">
             Your Ultimate destination for thrilling casino games
           </StyledLogoSubtitle>
         </StyledLogoContainer>
-        <StyledCta onClick={() => {}}>
+        <StyledCta className="unlock-cta" onClick={() => {}}>
           <CtaVector />
           <StyledCtaTextWrapper>Unlock Fun</StyledCtaTextWrapper>
         </StyledCta>
