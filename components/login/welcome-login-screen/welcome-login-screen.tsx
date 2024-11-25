9dd0e3ff988970dcede8b9d16fb9eda47f483a2d
= <=>
2024-11-25 12:54:14 +0530

    [logo-optimization] - updated animation in welcome login screen

--- components/login/welcome-login-screen/welcome-login-screen.tsx
+++ components/login/welcome-login-screen/welcome-login-screen.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 // types
 import type { FC } from "react";
 
@@ -40,6 +40,7 @@ import JackOLaternJson from "@/public/login/welcome-login-screen/jack-o-latern.j
 
 // gsap
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import MotionPathPlugin from "gsap/MotionPathPlugin";
 gsap.registerPlugin(MotionPathPlugin);
 
@@ -48,8 +49,122 @@ const WelcomeLoginScreen: FC<{
 }> = ({ updateShowLogin }) => {
   const page_ref = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const gsap_context = gsap.context(() => {
+  const { contextSafe } = useGSAP(
+    () => {
+      gsap
+        .timeline()
+        // spider web animation
+        .fromTo(
+          "#spider-web-vector",
+          {
+            opacity: 0,
+          },
+          {
+            opacity: 1,
+            duration: 1,
+          }
+        )
+        .fromTo(
+          "#web-vector",
+          {
+            opacity: 0,
+          },
+          {
+            opacity: 1,
+            duration: 0.5,
+          },
+          "<"
+        )
+
+        // logo animation
+        .fromTo(
+          ".logo-span",
+          {
+            opacity: 0,
+          },
+          {
+            opacity: 1,
+            duration: 1,
+            stagger: {
+              each: 0.05,
+              from: "random",
+              ease: "steps(6)",
+            },
+          }
+        )
+        .fromTo(
+          ".subtitle-span",
+          {
+            opacity: 0,
+            rotate: -60,
+          },
+          {
+            opacity: 1,
+            rotate: 0,
+            duration: 0.6,
+            stagger: 0.02,
+          }
+        )
+        .addLabel("logo-animation-completion")
+        .fromTo(
+          "#main-image",
+          {
+            filter: "brightness(30%)",
+          },
+          {
+            filter: "brightness(100%)",
+            ease: "expo.in",
+            duration: 1,
+          },
+          "logo-animation-completion-=1"
+        )
+        .fromTo(
+          "#banner-container",
+          {
+            scale: 0.5,
+            opacity: 0,
+          },
+          {
+            scale: 1,
+            opacity: 1,
+            ease: "elastic.inOut",
+            duration: 1.5,
+          },
+          "logo-animation-completion-=1"
+        )
+        .fromTo(
+          "#main-cta",
+          {
+            opacity: 0,
+          },
+          {
+            opacity: 1,
+            duration: 1,
+            ease: "expo",
+          },"<"
+        )
+        .fromTo(
+          "#haunted-car",
+          {
+            right: -500,
+          },
+          {
+            right: -140,
+            duration: 1,
+          }
+        );
+
+      // jack o latern animation
+      gsap.to("#jack-o-latern", {
+        motionPath: {
+          path: "M0.5 254C0.5 254 165.841 163.755 261.5 204C364.226 247.218 301.953 412.278 407 449.5C526.439 491.822 601.424 385.891 695.5 301C752.916 249.19 748.857 177.324 820 147C933.454 98.6406 1025.75 187.921 1105.5 282C1149.6 334.026 1177.2 369.745 1185 437.5C1195.13 525.522 1173.02 592.629 1105.5 650C1006.26 734.316 886.99 555.15 773.5 619C720.192 648.991 716.713 701.177 662.5 729.5C539.29 793.87 397.444 752.42 318.5 638C275.442 575.592 263.992 524.897 272 449.5C285.351 323.789 518.3 325.39 483 204C467.162 149.535 385.912 146.277 393 90C399.985 34.5393 452.944 20.5419 505.5 1.49998C609.127 -36.0463 663.336 93.4873 773.5 90C874.078 86.8161 915.34 -8.20043 1015.5 1.49998C1141.77 13.729 1269.5 204 1269.5 204",
+        },
+        repeat: -1,
+        duration: 20,
+        ease: "power1.in",
+        yoyo: true,
+      });
+
       /**
        * Finger print animation
        */
@@ -85,127 +200,18 @@ const WelcomeLoginScreen: FC<{
           ease: "steps(6)",
         }
       );
+    },
+    { scope: page_ref }
+  );
 
-      gsap
-        .timeline()
-        .addLabel("start")
-        .fromTo(
-          "#spider-web-vector",
-          {
-            opacity: 0,
-          },
-          {
-            opacity: 1,
-            duration: 1,
-          }
-        )
-        .fromTo(
-          "#web-vector",
-          {
-            opacity: 0,
-          },
-          {
-            opacity: 1,
-            duration: 0.5,
-          },
-          "<"
-        )
-        .fromTo(
-          "#main-image",
-          {
-            filter: "brightness(30%)",
-          },
-          {
-            filter: "brightness(100%)",
-            ease: "expo.in",
-            duration: 1,
-          }
-        )
-        .fromTo(
-          "#haunted-car",
-          {
-            right: -500,
-          },
-          {
-            right: -140,
-            duration: 1,
-          }
-        )
-        .fromTo(
-          "#banner-container",
-          {
-            scale: 0.5,
-            opacity: 0,
-          },
-          {
-            scale: 1,
-            opacity: 1,
-            ease: "elastic.inOut",
-            duration: 1.5,
-          },
-          "start+=1"
-        );
-    }, page_ref);
-    return () => {
-      gsap_context.revert();
-    };
-  }, []);
-
-  // logo animation
-  useEffect(() => {
-    const gsap_context = gsap.context(() => {
-      gsap
-        .timeline()
-        .fromTo(
-          ".logo-span",
-          {
-            opacity: 0,
-          },
-          {
-            opacity: 1,
-            duration: 1,
-            stagger: {
-              each: 0.05,
-              from: "random",
-              ease: "steps(6)",
-            },
-          }
-        )
-        .fromTo(
-          ".subtitle-span",
-          {
-            opacity: 0,
-            rotate: -60,
-          },
-          {
-            opacity: 1,
-            rotate: 0,
-            duration: 0.6,
-            stagger: 0.02,
-          }
-        );
-    }, page_ref);
-    return () => {
-      gsap_context.revert();
-    };
-  }, []);
-
-  useEffect(() => {
-    const gsap_context = gsap.context(() => {
-      gsap.to("#jack-o-latern", {
-        motionPath: {
-          path: "M0.5 254C0.5 254 165.841 163.755 261.5 204C364.226 247.218 301.953 412.278 407 449.5C526.439 491.822 601.424 385.891 695.5 301C752.916 249.19 748.857 177.324 820 147C933.454 98.6406 1025.75 187.921 1105.5 282C1149.6 334.026 1177.2 369.745 1185 437.5C1195.13 525.522 1173.02 592.629 1105.5 650C1006.26 734.316 886.99 555.15 773.5 619C720.192 648.991 716.713 701.177 662.5 729.5C539.29 793.87 397.444 752.42 318.5 638C275.442 575.592 263.992 524.897 272 449.5C285.351 323.789 518.3 325.39 483 204C467.162 149.535 385.912 146.277 393 90C399.985 34.5393 452.944 20.5419 505.5 1.49998C609.127 -36.0463 663.336 93.4873 773.5 90C874.078 86.8161 915.34 -8.20043 1015.5 1.49998C1141.77 13.729 1269.5 204 1269.5 204",
-        },
-        repeat: -1,
-        duration: 20,
-        ease: "power1.in",
-        yoyo: true,
-      });
-    }, page_ref);
-    return () => {
-      gsap_context.revert();
-    };
-  }, []);
+  const mouseEventHandler = contextSafe((event_type: "enter" | "leave") => {
+    gsap.to("#main-cta", {
+      rotate: event_type == "enter" ? 2 : 0,
+      scale: event_type == "enter" ? 1.05 : 1,
+      duration: 1,
+      ease: "elastic",
+    });
+  });
 
   return (
     <StyledPage ref={page_ref}>
@@ -305,19 +311,23 @@ const WelcomeLoginScreen: FC<{
           <StyledRightContentContainer>
             <StyledLogoContainer>
               <StyledLogo>
-                {"Fortune Realm".split("").map((char) => {
-                  return <span className="logo-span">{char}</span>;
+                {"Fortune Realm".split("").map((char, index) => {
+                  return (
+                    <span key={`logo-${index}`} className="logo-span">
+                      {char}
+                    </span>
+                  );
                 })}
               </StyledLogo>
               <StyledSubTitle>
                 {"Your ultimate destination for thrilling casino games"
                   .split("")
-                  .map((char) => {
-                    if (char == " ") {
-                      console.log("teting");
-                    }
+                  .map((char, index) => {
                     return (
-                      <StyledSubTitleSpan className="subtitle-span">
+                      <StyledSubTitleSpan
+                        className="subtitle-span"
+                        key={`subtitle-${index}`}
+                      >
                         {char == " " ? "\u00A0" : char}
                       </StyledSubTitleSpan>
                     );
@@ -371,7 +381,12 @@ const WelcomeLoginScreen: FC<{
                 </StyledPokerCardWrapper>
               </ThemeProvider>
             </StyledGameBannerContainer>
-            <StyledPlayCta onClick={() => updateShowLogin(true)}>
+            <StyledPlayCta
+              id="main-cta"
+              onMouseEnter={() => mouseEventHandler("enter")}
+              onMouseLeave={() => mouseEventHandler("leave")}
+              onClick={() => updateShowLogin(true)}
+            >
               <StyledPlayCtaVector>
                 <LoginPlayCtaVector is_mobile={false} />
               </StyledPlayCtaVector>
