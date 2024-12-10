e7286a5cae906a638a189777a6b726a1774ba4df
= <=>
2024-12-10 10:54:22 +0530

    [login-optimization] add jack o latern animation in mobile view

--- styles/components/login/welcome-login-screen/mobile-welcome-login-screen.style.ts
+++ styles/components/login/welcome-login-screen/mobile-welcome-login-screen.style.ts
@@ -125,6 +125,27 @@ const StyledPlayCta = styled.div`
 const StyledPlayCtaVector = styled.div`
   position: absolute;
 `;
+
+const StyledLottiePlayerContainer = styled.div<{
+  $top?: string;
+  $bottom?: string;
+  $left?: string;
+  $right?: string;
+  $translateX?: string;
+  $translateY?: string;
+}>`
+  width: auto;
+  height: auto;
+  position: absolute;
+  top: ${(props) => props.$top ?? "auto"};
+  bottom: ${(props) => props.$bottom ?? "auto"};
+  left: ${(props) => props.$left ?? "auto"};
+  right: ${(props) => props.$right ?? "auto"};
+  transform: translate(
+    ${(props) => props.$translateX ?? 0},
+    ${(props) => props.$translateY ?? 0}
+  );
+`;
 export {
   StyledPage,
   StyledImageContainer,
@@ -137,4 +158,5 @@ export {
   StyledBottomShadow,
   StyledPlayCta,
   StyledPlayCtaVector,
+  StyledLottiePlayerContainer,
 };
